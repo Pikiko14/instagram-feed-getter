ad1d19af36d1406a8270cd90735f0f3d33144f06
Marcos Hernandez <maalhequi95@gmail.com>
2025-07-08 16:29:23 -0500

    enable cors for admin panel

--- src/middlewares/tokenValidator.js
+++ src/middlewares/tokenValidator.js
@@ -1,5 +1,23 @@
 const tokenValidator = async (req, res, next) => {
-  next();
+  try {
+    const { host } = req.headers;
+
+    if (!host) {
+      return res
+        .status(403)
+        .json({ message: "No puedes acceder a esta solicitud" });
+    }
+
+    if (host !== 'back.motowork.co') {
+      return res
+        .status(403)
+        .json({ message: "No puedes acceder a esta solicitud" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(403).send("No puedes acceder a esta solicitud");
+  }
 };
 
 module.exports = tokenValidator;
